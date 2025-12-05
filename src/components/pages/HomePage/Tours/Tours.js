4f5c9cdf91a2e7a1fc8fbed7a4606c886711abe4
Alex <tor.trapeczia@gmail.com>
2025-12-05 10:34:33 +0300

    add Tours section

--- src/components/pages/HomePage/Tours/Tours.js
+++ src/components/pages/HomePage/Tours/Tours.js
@@ -1,157 +1,96 @@
+import styles from './tours.module.scss'
+import cardStyles from './card.module.scss'
+import filterStyles from './filter.module.scss'
+import linkStyle from './link.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useState } from 'react'
 
+function Card({ id, title, subtitle, img }) {
+    return (
+        <div className={cardStyles.card}>
+            <img
+                className={cardStyles.card__img}
+                src={img}
+                alt=""
+                loading="lazy"
+            />
+            <div className={cardStyles.card__top}>
+                <h3>{title}</h3>
+                <p>{subtitle}</p>
+            </div>
+            <div className={cardStyles.card__bottom}>
+                <Link className={cardStyles.card__button} href="#">
+                    <span className='p-big'>Подробнее</span>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+function Button({ label, onClick, isActive }) {
+    return (
+        <button
+            className={
+                `${styles['filter-list__button']} 
+                ${linkStyle.link} 
+                ${linkStyle['link--filter']}
+                ${isActive ? linkStyle['link--selected'] : ''}
+                p-big`
+            }
+            type="button"
+            onClick={onClick}
+        >
+            {label}
+        </button>
+    )
+}
+
+function FilterList({ filter_list, className = '' }) {
+    const [activeId, setActiveId] = useState(filter_list[0]?.id)
+    return (
+        <ul className={`${filterStyles['filter-list']} ${className}`}>
+            {filter_list.map((item) =>
+                <li className={`filter-list__item`} key={item.id}>
+                    <Button
+                        label={item.label}
+                        onClick={() => setActiveId(item.id)} 
+                        isActive={activeId === item.id}/>
+                </li>
+            )}
+        </ul>
+    )
+}
 
 export default function Tours() {
+    const filter_list = [
+        { id: 1, label: 'Популярные' },
+        { id: 2, label: 'Авторские' },
+        { id: 3, label: 'Походы' },
+        { id: 4, label: 'Сплавы' },
+        { id: 5, label: 'Велопрогулки' }
+    ]
+    const cards = [
+        { id: 1, title: 'Путешествие в горы', subtitle: 'от 80 000 руб', img: 'images/card-tour-photo-1.png' },
+        { id: 2, title: 'Путешествие в горы', subtitle: 'от 80 000 руб', img: 'images/card-tour-photo-2.png' },
+        { id: 3, title: 'Путешествие в горы', subtitle: 'от 80 000 руб', img: 'images/card-tour-photo-3.png' },
+        { id: 4, title: 'Путешествие в горы', subtitle: 'от 80 000 руб', img: 'images/card-tour-photo-4.png' },
+        { id: 5, title: 'Путешествие в горы', subtitle: 'от 80 000 руб', img: 'images/card-tour-photo-5.jpg' },
+        { id: 6, title: 'Путешествие в горы', subtitle: 'от 80 000 руб', img: 'images/card-tour-photo-6.png' },
+    ]
     return (
-        <section className="section" id="tour-choose">
-            <header className="section__header">
-                <h2 className="section__title">Выбери свой тур</h2>
-                <ul className="section__filter-list filter-list p-big">
-                    <li className="filter-list__item">
-                        <button
-                            className="filter-list__button link link--filter link--selected"
-                            type="button"
-                        >
-                            Популярные
-                        </button>
-                    </li>
-                    <li className="filter-list__item">
-                        <button className="filter-list__button link link--filter" type="button">
-                            Авторские
-                        </button>
-                    </li>
-                    <li className="filter-list__item">
-                        <button className="filter-list__button link link--filter" type="button">
-                            Походы
-                        </button>
-                    </li>
-                    <li className="filter-list__item">
-                        <button className="filter-list__button link link--filter" type="button">
-                            Сплавы
-                        </button>
-                    </li>
-                    <li className="filter-list__item">
-                        <button className="filter-list__button link link--filter" type="button">
-                            Велопрогулки
-                        </button>
-                    </li>
-                </ul>
+        <section className={styles.section} id='tour-choose'>
+            <header className={styles.section__header}>
+                <h2 className={styles.section__title}>Выбери свой тур</h2>
+                <FilterList filter_list={filter_list} className={styles['section__filter-list']} />
             </header>
-            <div className="section__content tour-grid">
-                <ul className="tour-grid__list">
-                    <li className="tour-grid__item">
-                        <div className="tour-grid__card card">
-                            <img
-                                className="card__img"
-                                src="images/card-tour-photo-1.png"
-                                alt=""
-                                loading="lazy"
-                            />
-                            <div className="card__top">
-                                <h3 className="card__title">Путешествие в горы</h3>
-                                <p className="card__subtitle p-small">от 80 000 руб</p>
-                            </div>
-                            <div className="card__bottom">
-                                <a className="card__button p-big" href=".">
-                                    Подробнее
-                                </a>
-                            </div>
-                        </div>
-                    </li>
-                    <li className="tour-grid__item">
-                        <div className="tour-grid__card card">
-                            <img
-                                className="card__img"
-                                src="images/card-tour-photo-2.png"
-                                alt=""
-                                loading="lazy"
-                            />
-                            <div className="card__top">
-                                <h3 className="card__title">Путешествие в горы</h3>
-                                <p className="card__subtitle p-small">от 80 000 руб</p>
-                            </div>
-                            <div className="card__bottom">
-                                <a className="card__button p-big" href=".">
-                                    Подробнее
-                                </a>
-                            </div>
-                        </div>
-                    </li>
-                    <li className="tour-grid__item">
-                        <div className="tour-grid__card card">
-                            <img
-                                className="card__img"
-                                src="images/card-tour-photo-3.png"
-                                alt=""
-                                loading="lazy"
-                            />
-                            <div className="card__top">
-                                <h3 className="card__title">Путешествие в горы</h3>
-                                <p className="card__subtitle p-small">от 80 000 руб</p>
-                            </div>
-                            <div className="card__bottom">
-                                <a className="card__button p-big" href=".">
-                                    Подробнее
-                                </a>
-                            </div>
-                        </div>
-                    </li>
-                    <li className="tour-grid__item">
-                        <div className="tour-grid__card card">
-                            <img
-                                className="card__img"
-                                src="images/card-tour-photo-4.png"
-                                alt=""
-                                loading="lazy"
-                            />
-                            <div className="card__top">
-                                <h3 className="card__title">Путешествие в горы</h3>
-                                <p className="card__subtitle p-small">от 80 000 руб</p>
-                            </div>
-                            <div className="card__bottom">
-                                <a className="card__button p-big" href=".">
-                                    Подробнее
-                                </a>
-                            </div>
-                        </div>
-                    </li>
-                    <li className="tour-grid__item">
-                        <div className="tour-grid__card card">
-                            <img
-                                className="card__img"
-                                src="images/card-tour-photo-5.jpg"
-                                alt=""
-                                loading="lazy"
-                            />
-                            <div className="card__top">
-                                <h3 className="card__title">Путешествие в горы</h3>
-                                <p className="card__subtitle p-small">от 80 000 руб</p>
-                            </div>
-                            <div className="card__bottom">
-                                <a className="card__button p-big" href=".">
-                                    Подробнее
-                                </a>
-                            </div>
-                        </div>
-                    </li>
-                    <li className="tour-grid__item">
-                        <div className="tour-grid__card card">
-                            <img
-                                className="card__img"
-                                src="images/card-tour-photo-6.png"
-                                alt=""
-                                loading="lazy"
-                            />
-                            <div className="card__top">
-                                <h3 className="card__title">Путешествие в горы</h3>
-                                <p className="card__subtitle p-small">от 80 000 руб</p>
-                            </div>
-                            <div className="card__bottom">
-                                <a className="card__button p-big" href=".">
-                                    Подробнее
-                                </a>
-                            </div>
-                        </div>
-                    </li>
+            <div className={`${styles.section__content} ${styles['tour-grid']}`}>
+                <ul className={styles['tour-grid__list']}>
+                    {cards.map((item) =>
+                        <li key={item.id}>
+                            <Card {...item} />
+                        </li>
+                    )}
                 </ul>
             </div>
         </section>
