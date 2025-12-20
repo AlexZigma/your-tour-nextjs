302e3b8b77b1a65dfbaf31f95aa5f39875bb3de8
Alex <tor.trapeczia@gmail.com>
2025-12-20 14:57:02 +0300

    change FilterList: change to FilterButton

--- src/components/commons/Filter/FilterList.js
+++ src/components/commons/Filter/FilterList.js
@@ -1,26 +1,20 @@
-import AppLink from '../AppLink/AppLink'
 import styles from './filter.module.scss'
 import { useState } from 'react'
-
+import FilterButton from './FilterButton'
 
 export default function FilterList({ filter_list, className = '' }) {
     const [activeId, setActiveId] = useState(filter_list[0]?.id)
+
     return (
         <ul className={`${styles['filter-list']} ${className}`}>
             {filter_list.map((item) =>
-                <li className={`filter-list__item`} key={item.id}>
-                    <AppLink
-                        variant={'filter'}
-                        className={`
-                            ${styles['filter-list__button']} 
-                            p-big
-                        `}
-                        href={item.href}
-                        onClick={() => setActiveId(item.id)}
+                <li key={item.id}>
+                    <FilterButton
                         isActive={activeId === item.id}
+                        onClick={() => setActiveId(item.id)}
                     >
                         {item.label}
-                    </AppLink>
+                    </FilterButton>
                 </li>
             )}
         </ul>
