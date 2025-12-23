3ed3f72928bec5059582892bbb4b9b28c31fa1a6
Alex <tor.trapeczia@gmail.com>
2025-12-23 11:04:50 +0300

    fix Reviews: review text formatting

--- src/components/pages/HomePage/Reviews/ReviewCard.js
+++ src/components/pages/HomePage/Reviews/ReviewCard.js
@@ -5,7 +5,7 @@ export default function ReviewCard({ title, subtitle, img, children }) {
     return (
         <div className={`${styles['review-card']}`}>
             <div className={`${['review-card__top']}`}>
-                <p className={`p-big ${styles['review-card__text']}`}>
+                <p className={`p-big`}>
                     {children}
                 </p>
             </div>
