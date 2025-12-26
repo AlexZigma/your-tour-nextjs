3d51c013d03e74faa0b282a3785c627a2aac8f36
Alex <tor.trapeczia@gmail.com>
2025-12-26 14:52:55 +0300

    Add _document: meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>YourTour</title>
+        <meta name="description" content="Идейные соображения высшего порядка, а также рамки и место обучения кадров" />
+        <meta name="keywords" content="путешествия, туры, отдых, билеты, экскурсии, море, горы, пляж, туризм, выбрать тур" />
+      </Head>
       <body>
         <Main />
         <NextScript />
