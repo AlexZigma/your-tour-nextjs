c34078ebf7aaaa41b46ea54e38247a737f8a499d
Alex <tor.trapeczia@gmail.com>
2025-12-28 21:57:31 +0300

    change Picture: add props

--- src/components/commons/Picture/Picture.js
+++ src/components/commons/Picture/Picture.js
@@ -1,15 +1,17 @@
 import styles from './Picture.module.scss'
 import Image from 'next/image'
 
-export default function Picture({src, className}) {
+export default function Picture({ src, className, alt = '', ...rest }) {
     return (
         <div className={`${styles.picture} ${className}`}>
             <Image
                 className={`${styles.picture__img}`}
                 src={src}
                 fill
-                alt=""
+                alt={alt}
                 sizes="(max-width: 1024px) 100vw, 450px"
+                {...rest}
+                loading='lazy'
             />
         </div>
     )
