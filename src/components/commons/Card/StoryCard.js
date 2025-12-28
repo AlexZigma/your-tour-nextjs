4dceda0d2ed64d5f307db22b0f35fd9bb44d9285
Alex <tor.trapeczia@gmail.com>
2025-12-28 21:57:06 +0300

    add StoryCard: new component

---
+++ src/components/commons/Card/StoryCard.js
@@ -0,0 +1,30 @@
+import styles from './TourCard.module.scss'
+import Link from 'next/link'
+import Image from 'next/image';
+import AppLink from '../AppLink/AppLink';
+import AnimatedCard from './AnimatedCard';
+
+export default function StoryCard({ title, img, href, children, links }) {
+    return (
+        <AnimatedCard img={img} className={styles['card--big']}>
+            <div className={styles.card__top}>
+                <h3>{title}</h3>
+                {children && <div className='p-normal'>{children}</div>}
+            </div>
+            <div className={styles.card__bottom}>
+                <Link className={`${styles.card__button} p-big`} href={href}>
+                    Подробнее
+                </Link>
+                {links &&
+                    <div className={`${styles.card__socials} p-normal`}>
+                        {links?.map((item) =>
+                            <AppLink variant={'social'} href={item.href} key={item.id}>
+                                {item.label}
+                            </AppLink>
+                        )}
+                    </div>
+                }
+            </div>
+        </AnimatedCard>
+    )
+}
